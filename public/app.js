ea443d5a7bbdc4aa02e6d3add25bdf7a1ef158e3
Tare O <oyinetare@Tares-MacBook-Pro.local>
2019-12-28 01:32:28 +0000

    make some functions async so code is cleaner, still only in console for now

--- public/app.js
+++ public/app.js
@@ -18,68 +18,77 @@ document.addEventListener('DOMContentLoaded', () => {
 })
 
 function processQuery(query) {
-    let artisteUrl = `http://musicbrainz.org/ws/2/artist/?query=artist:${query}&limit=1&fmt=json`; //return top artist
+    const artisteUrl = `http://musicbrainz.org/ws/2/artist/?query=artist:${query}&limit=1&fmt=json`; //return top artist
 
     fetch(artisteUrl)
-        .then(r => r.json())
-        .then((r) => {
-            let limit = 100
+    .then(r => r.json())
+    .then((r) => {
+        let limit = 100; let artist = r.artists[0].id
 
-            let albumUrl = `http://musicbrainz.org/ws/2/release-group/?query=release-group:%20*%20AND%20arid:${r.artists[0].id}%20AND%20status:official&limit=${limit}&fmt=json`;
-            let releasGroups = [];
-            
-           return keepFetching(albumUrl, releasGroups, limit, r.artists[0].id)
-        })
-        .then(releaseGroups => {
-            let recordings = new Set(); let releases = [];
-    
-            releaseGroups.forEach(releaseGroup => {
-                releaseGroup.releases.forEach(release => {
-                    releases.push(release); //to make quering musicbrainz for each individual release easier
-                })
+        const albumUrl = `http://musicbrainz.org/ws/2/release-group/?query=release-group:%20*%20AND%20arid:${artist}%20AND%20status:official&limit=${limit}&fmt=json`;
+        let releasGroups = [];
+        
+        return keepFetching(albumUrl, releasGroups, limit, artist);
+    })
+    .then(releaseGroups => {
+        let recordings = new Map(); let releases = [];
+
+        releaseGroups.forEach(releaseGroup => {
+            releaseGroup.releases.forEach(release => {
+                //add to array to make quering musicbrainz for each individual release easier
+                releases.push(release);
             })
-
-            releases.forEach(delayLoop(release => {
-                getRecordings(recordings, release);
-            }, 1000))
         })
-        .catch(e => console.error(`error finding artist ${e}`))
+
+        releases.forEach(delayLoop(release => {
+            getRecordings(recordings, release);
+        }, 1000))
+    })
+    .catch(e => console.error(`error finding artist ${e}`))
         //finally
 }
 
-function keepFetching(url, releaseGroups, count, artist){
-    return fetch(url)
-    .then(r => r.json())
-    .then(r => {
+async function keepFetching(url, releaseGroups, count, artist){
+
+    try {
+        let res = await fetch(url);
+        let r = await res.json();
+
         releaseGroups = releaseGroups.concat(r['release-groups']);
 
         if(r.count > count) {
-            
-            let albumUrl = `http://musicbrainz.org/ws/2/release-group/?query=release-group:%20*%20AND%20arid:${artist}%20AND%20status:official&limit=100&offset=${count}&fmt=json`
+            const albumUrl = `http://musicbrainz.org/ws/2/release-group/?query=release-group:%20*%20AND%20arid:${artist}%20AND%20status:official&limit=100&offset=${count}&fmt=json`
             count+=100
 
             return keepFetching(albumUrl, releaseGroups, count, artist)
         }
+
         return releaseGroups;
-    })
-    .catch(e => console.error(`error fetching from ${url} || error mesage -> ${e}`))
+    }
+    catch (e) {
+        console.error(`error fetching from ${url} || error mesage -> ${e}`)
+    }
+
 }
 
-function getRecordings(recordings, release){
-    let recordingUrl = `http://musicbrainz.org/ws/2/recording/?query=reid:${release.id}&limit=100&fmt=json`
+async function getRecordings(recordings, release){
+    const recordingUrl = `http://musicbrainz.org/ws/2/recording/?query=reid:${release.id}&limit=100&fmt=json`
 
-    fetch(recordingUrl)
-    .then(r => r.json())
-    .then(r => {
+    try {
+        let res = await fetch(recordingUrl);
+        let r = await res.json();
+
         r.recordings.forEach(recording =>{
-            recordings.add(recording.title);
+            //add owner of record, in case artist has been featured - useful when searching for lyrics
+            recordings.set(recording.title, recording['artist-credit'][0].name); 
         })
-    })
-    .catch(e => console.error(`error finding record ${e}`))
-    //finally
 
-    console.log(recordings);
-    return recordings;
+        console.log(recordings)
+        return recordings;
+    }
+    catch (e) {
+        console.error(`failed to get recordings for ${release.title} || error mesage -> ${e}`)
+    }
 }
 
 function delayLoop(fn, delay){
